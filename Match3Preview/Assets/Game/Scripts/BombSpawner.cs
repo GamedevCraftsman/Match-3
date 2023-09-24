16ab38ad220e99538eb64869408620a3c6beec2d
Danylo <ddssoolloovvkkoo4400@gmail.com>
2023-09-24 17:25:43 +0300

    commit 5

--- Match3Preview/Assets/Game/Scripts/BombSpawner.cs
+++ Match3Preview/Assets/Game/Scripts/BombSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class BombSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private GameObject parent;
     [SerializeField] private float[,] startPoint = {{-3.2f},{-3.15f}}; // where start cloning.
     [SerializeField] private float[,] endPoint = {{3.05f},{3.1f}}; // where end cloning.
 
@@ -17,7 +18,7 @@ public class BombSpawner : MonoBehaviour
     void FillIn(){
         for(float i = startPoint[0,0]; i <= endPoint[0,0]; i += difference){
             for(float j = startPoint[1,0]; j <= endPoint[1,0]; j += difference){
-                Instantiate(prefabs[UnityEngine.Random.Range(0, prefabs.Count())], new Vector3(i,j,0), Quaternion.identity);      
+                Instantiate(prefabs[UnityEngine.Random.Range(0, prefabs.Count())], new Vector3(i,j,0), Quaternion.identity, parent.transform);      
             }
         }
     }
