3666d44b22bcbac537b274d589c82ad84b06768c
Danylo <ddssoolloovvkkoo4400@gmail.com>
2023-09-24 16:53:07 +0300

    commit 3

---
+++ Match3Preview/Assets/Game/Scripts/BombSpawner.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using UnityEngine;
+
+[ExecuteAlways] // This line give opportunity to play in edit mode.
+public class BombSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private float[,] startPoint = {{-3.2f},{-3.15f}}; // where start cloning.
+    [SerializeField] private float[,] endPoint = {{3.05f},{3.1f}}; // where end cloning.
+
+    private float difference = 1.25f; // distance between spawnpoints.
+    
+    void Start()
+    {
+        FillIn(); // function which fill in chart.
+    }
+
+    void FillIn(){
+        for(float i = startPoint[0,0]; i <= endPoint[0,0]; i += difference){
+            for(float j = startPoint[1,0]; j <= endPoint[1,0]; j += difference){
+                Instantiate(prefabs[UnityEngine.Random.Range(0, prefabs.Count())], new Vector3(i,j,0), Quaternion.identity);      
+            }
+        }
+    }
+
+#if UNITY_EDITOR //code after this line will lose after compilation.
+    [ContextMenu("Spawn Bombs")]
+    void Awake(){
+        if (Application.isPlaying){
+        Destroyer();
+        }
+    }
+
+    void Destroyer(){ // function destroy objects which appear in edit mode.
+        GameObject[] obs = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach(GameObject ob in obs){
+            Destroy(ob);
+        }
+
+    }
+#endif
+}
