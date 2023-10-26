d8617a89b1555fa7df030d0534f49bffdd144f14
Danylo <ddssoolloovvkkoo4400@gmail.com>
2023-10-26 15:33:26 +0300

    Майже все виконано, але є баг :c

--- Match3Preview/Assets/Game/Scripts/GroupingAndDeleting.cs
+++ Match3Preview/Assets/Game/Scripts/GroupingAndDeleting.cs
@@ -2,21 +2,88 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
+using UnityEditor;
+using System.Data;
+
 public class GroupingAndDeleting : MonoBehaviour, IPointerClickHandler
 {
     public enum bombsColor { Blue, Red, Green, Orange, Purple, Nothing };
     public bombsColor color;
 
+    const int offset = 1;
+    const int columnSize = 6;
+    const int numberOfNeighbors = 1;
+    int[] farBombsIndexes = { 5, 6, 11, 12, 17, 18, 23, 34, 29, 30 };
     List<GameObject> bombsForDelete = new List<GameObject>();
     int clickedCellIndex = -1;
     CellScript[] cells;
     GameObject cellContainer;
-    private void Start()
+    void Start()
     {
         cellContainer = GameObject.FindGameObjectWithTag("cellContainer");
         cells = cellContainer.GetComponent<CellContainer>().cells;
     }
     public void OnPointerClick(PointerEventData eventData)
+    {
+        FindClickedCellIndex();
+
+        CheckAndAddNeighborToList(-offset);
+        CheckAndAddNeighborToList(offset);
+        CheckAndAddNeighborToList(-columnSize);
+        CheckAndAddNeighborToList(columnSize);
+
+        CheckingTheNumberAndDeleteNeighbors();
+    }
+
+    void CheckAndAddNeighborToList(int offset)
+    {
+        int neighborIndex = clickedCellIndex + offset;
+
+        if (CheckBorders(neighborIndex) && IsNeighborMatchingColor(neighborIndex))
+        {
+            bombsForDelete.Add(cells[neighborIndex].bomb);
+            cells[neighborIndex].isFree = true;
+        }
+        CheckingFarBombs(neighborIndex);    
+    }
+
+    bool CheckBorders(int neighborIndex)
+    {
+        return (neighborIndex < cells.Length && neighborIndex >= 0);
+    }
+
+    bool IsNeighborMatchingColor(int neighborIndex)
+    {
+        if (cells[neighborIndex].bomb.GetComponent<GroupingAndDeleting>().color == color)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void CheckingTheNumberAndDeleteNeighbors()
+    {
+        if (bombsForDelete.Count() >= numberOfNeighbors)
+        {
+            DeleteBombsFromList();
+        }
+        bombsForDelete.Clear();
+    }
+
+    void DeleteBombsFromList()
+    {
+        foreach (var bomb in bombsForDelete)
+        {
+            cells[clickedCellIndex].isFree = true;           
+            Destroy(bomb);
+            Destroy(gameObject);           
+        }
+    }
+
+    void FindClickedCellIndex()
     {
         for (int i = 0; i < cells.Length; i++)
         {
@@ -26,39 +93,33 @@ public class GroupingAndDeleting : MonoBehaviour, IPointerClickHandler
                 break;
             }
         }
-        if (clickedCellIndex - 1 >= 0 && cells[clickedCellIndex - 1].bomb.GetComponent<GroupingAndDeleting>().color == color)
-        {
-            cells[clickedCellIndex - 1].isFree = true;
-            bombsForDelete.Add(cells[clickedCellIndex - 1].bomb);
-        }
-        if (clickedCellIndex + 1 < cells.Length && cells[clickedCellIndex + 1].bomb.GetComponent<GroupingAndDeleting>().color == color)
-        {
-            cells[clickedCellIndex + 1].isFree = true;
-            bombsForDelete.Add(cells[clickedCellIndex + 1].bomb);
+    }
 
-        }
-        if (clickedCellIndex + 6 < cells.Length && cells[clickedCellIndex + 6].bomb.GetComponent<GroupingAndDeleting>().color == color)
+    void CheckingFarBombs(int neighborIndex)
+    {
+        for (int i = 0; i < farBombsIndexes.Length; i++)
         {
-            cells[clickedCellIndex + 6].isFree = true;
-            bombsForDelete.Add(cells[clickedCellIndex + 6].bomb);
+            CheckUpBombs(neighborIndex, i);
+            CheckDownBombs(neighborIndex, i);
+        }
+    }
 
-        }
-        if (clickedCellIndex - 6 >= 0 && cells[clickedCellIndex - 6].bomb.GetComponent<GroupingAndDeleting>().color == color)
+    void CheckUpBombs(int neighborIndex, int i)
+    {
+        if (CheckBorders(neighborIndex) && neighborIndex == clickedCellIndex + 1 && neighborIndex == farBombsIndexes[i])
         {
-            cells[clickedCellIndex - 6].isFree = true;
-            bombsForDelete.Add(cells[clickedCellIndex - 6].bomb);
+            bombsForDelete.Remove(cells[clickedCellIndex + offset].bomb);
+            cells[clickedCellIndex + offset].isFree = false;
         }
+    }
 
-        if (bombsForDelete.Count() >= 1)
+    void CheckDownBombs(int neighborIndex, int i)
+    {
+        if (CheckBorders(neighborIndex) && neighborIndex == clickedCellIndex - 1 && neighborIndex == farBombsIndexes[i])
         {
-            foreach(var bomb in bombsForDelete)
-            {
-                Destroy(bomb);
-                Destroy(gameObject);
-                cells[clickedCellIndex].isFree = true;
-            }
+            bombsForDelete.Remove(cells[clickedCellIndex - offset].bomb);
+            cells[clickedCellIndex + offset].isFree = false;
         }
-        bombsForDelete.Clear();
     }
 }
 
